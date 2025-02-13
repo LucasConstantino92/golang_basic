42aabcda2d859c3468d981425446d3883a96e92a
Lucas Constantino <lucasconstantino0102@gmail.com>
2025-02-13 14:23:34 -0300

    External package

--- 1 - Packages/main.go
+++ 1 - Packages/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing from main")
 	auxiliar.Write()
+
+	erro := checkmail.ValidateFormat("lconstantino@gmail.com")
+
+	fmt.Println(erro)
 }
